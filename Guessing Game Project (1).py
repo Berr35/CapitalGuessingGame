11bac7c58ebee7af8b0cd349e7c87e090909ebc2
Berr35 <150918206+Berr35@users.noreply.github.com>
2024-01-16 14:38:48 +0100

    Delete Guessing Game Project (1).py

--- Guessing Game Project (1).py
+++
@@ -1,5 +0,0 @@
-questions_answers = [
-  {'question': 'Turkey Capital City', 'answer':0, options:['Ankara', 'Istanbul', 'Izmir']}
-  {'question': 'Poland Capital City', 'answer':1, options:['Gdansk', 'Warsaw', 'Krakow']}
-  {'question': 'Germany Capital City', 'answer':2, options:['Hamburg', 'Munich', 'Berlin']}
-]
